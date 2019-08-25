b02c879212bf200eb7058015407bbcb284e1cd86
Christian Rittmann <crittmann@hotmail.com>
2019-08-24 22:27:01 -0500

    added sticky nav and animation to skills/projects when scrolled to

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,5 @@
+//Code to add .sticky to navbar after scroll
+//Source: https://www.w3schools.com/howto/howto_js_sticky_header.asp
 window.onscroll = function() {
   stickyNav();
 };
@@ -13,3 +15,35 @@ let stickyNav = () => {
     navbar.classList.remove("sticky");
   }
 };
+
+// Code to animate class="hidden" elements
+// Source: https://eddyerburgh.me/animate-elements-scrolled-view-vanilla-js
+var animateHTML = function() {
+  var elems;
+  var windowHeight;
+  function init() {
+    elems = document.querySelectorAll(".hidden");
+    windowHeight = window.innerHeight;
+    addEventHandlers();
+    checkPosition();
+  }
+  function addEventHandlers() {
+    window.addEventListener("scroll", checkPosition);
+    window.addEventListener("resize", init);
+  }
+  function checkPosition() {
+    for (var i = 0; i < elems.length; i++) {
+      var positionFromTop = elems[i].getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= 0) {
+        elems[i].className = elems[i].className.replace(
+          "hidden",
+          "fade-in-element"
+        );
+      }
+    }
+  }
+  return {
+    init: init
+  };
+};
+animateHTML().init();
