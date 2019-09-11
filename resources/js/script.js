1a4aca6687b09d7dd692162a89256443de1bfb98
Christian Rittmann <crittmann@hotmail.com>
2019-09-10 19:10:23 -0500

    added mobile hamburger menu and updated nav-links :hover,focus

--- resources/js/script.js
+++ resources/js/script.js
@@ -47,3 +47,27 @@ var animateHTML = function() {
   };
 };
 animateHTML().init();
+
+const mobileNav = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `nav-links-fade 0.5s ease-in forwards ${index /
+          7 +
+          0.4}s`;
+      }
+    });
+
+    burger.classList.toggle("toggle");
+  });
+};
+
+mobileNav();
